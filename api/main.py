2eddafe57d69053847eba581da58827688c1ca5c
Kelly Domico <kdomico@gmail.com>
2021-05-06 18:01:20 -0700

    Add keypair generator API

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from Forecaster import DataModel
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello there. This is a simple keypair generator."}
+
+@app.get("/generate_keys")
+async def generate_keys():
+    # Generate private and public keys
+    return {"private": "YOUR_PRIVATE_KEY", "public": "YOUR_PUBLIC_KEY"}
